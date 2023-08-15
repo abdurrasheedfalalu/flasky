eba70e9a5c3320640b7bd5ea63eea781a8f0b87f
Abdurrasheed <abdurrasheedfalaludaura@gmail.com>
2023-08-15 17:30:22 +0100

    authentication

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,19 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-# from flask_mail import Mail
+from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from config import config
 
 
+from flask_login import LoginManager
+
 bootstrap = Bootstrap()
-# mail = Mail()
+mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
-
+login_manager = LoginManager()
+login_manager.login_view = "auth.login"
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -19,13 +22,17 @@ def create_app(config_name):
 
     config[config_name].init_app(app)
     bootstrap.init_app(app)
-    # mail.init_app(app)
+    mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     # register blueprints
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
 
 
     # attach routes and custom error pages here
