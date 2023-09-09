d9875554d34e2e9480868815a404e0ee89de0045
Abdurrasheed <abdurrasheedfalaludaura@gmail.com>
2023-09-09 23:20:37 +0100

    account data correction

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -13,13 +13,13 @@ class LoginForm(FlaskForm):
 
 
 class RegistrationForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Length(1, 64),
+                                             Email()])
     username = StringField('Username', validators=[
                 DataRequired(), Length(1, 64),
                 Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
                 'Usernames must have only letters, numbers, dots or '
                 'underscores')])
-    email = StringField("Email", validators=[DataRequired(), Length(1, 64),
-                                             Email()])
     password = PasswordField("Password", validators=[
                 DataRequired(), EqualTo("password2", message="Password must match.")])
     password2 = PasswordField("Confirm password", validators=[DataRequired()])
@@ -30,6 +30,39 @@ class RegistrationForm(FlaskForm):
         if User.query.filter_by(email=email.data).first():
             raise ValidationError("Email already registered.")
         
-    def validate_email(self, username):
+    def validate_username(self, username):
         if User.query.filter_by(username=username.data).first():
             raise ValidationError("Username already in use.")
+
+
+class ChangePasswordForm(FlaskForm):
+    old_password = PasswordField('Old password', validators=[DataRequired()])
+    password = PasswordField('New password', validators=[
+        DataRequired(), EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('Confirm new password',
+                              validators=[DataRequired()])
+    submit = SubmitField('Update Password')
+
+
+class PasswordResetRequestForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64),
+                                             Email()])
+    submit = SubmitField('Reset Password')
+
+
+class PasswordResetForm(FlaskForm):
+    password = PasswordField('New Password', validators=[
+        DataRequired(), EqualTo('password2', message='Passwords must match')])
+    password2 = PasswordField('Confirm password', validators=[DataRequired()])
+    submit = SubmitField('Reset Password')
+
+
+class ChangeEmailForm(FlaskForm):
+    email = StringField('New Email', validators=[DataRequired(), Length(1, 64),
+                                                 Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Update Email Address')
+
+    def validate_email(self, email):
+        if User.query.filter_by(email=email.data.lower()).first():
+            raise ValidationError('Email already registered.')
